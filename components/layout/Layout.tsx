6f8699c1834192b43cb85cfe6127284b3db50906
Kholofelo Tladi <tladi.kholofelo@gmail.com>
2024-12-22 22:24:43 +0200

    Create components/layout/Layout.tsx

---
+++ components/layout/Layout.tsx
@@ -0,0 +1,15 @@
+// components/layout/Layout.tsx
+import Header from "./Header";
+import Footer from "./Footer";
+
+const Layout: React.FC = ({ children }) => {
+  return (
+    <>
+      <Header />
+      <main className="min-h-screen">{children}</main>
+      <Footer />
+    </>
+  );
+};
+
+export default Layout;
