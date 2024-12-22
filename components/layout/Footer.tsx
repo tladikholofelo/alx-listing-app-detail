e23c03e773c94d09386d3a7e62d6c3546660a3e6
Kholofelo Tladi <tladi.kholofelo@gmail.com>
2024-12-22 22:24:25 +0200

    Create components/layout/Footer.tsx

---
+++ components/layout/Footer.tsx
@@ -0,0 +1,12 @@
+// components/layout/Footer.tsx
+import React from "react";
+
+const Footer: React.FC = () => {
+  return (
+    <footer className="bg-gray-800 text-white p-4 text-center">
+      <p>&copy; 2024 Property Listings. All rights reserved.</p>
+    </footer>
+  );
+};
+
+export default Footer;
