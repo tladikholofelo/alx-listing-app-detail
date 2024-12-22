dcdcb193762854ccb345d62f9af2ac324b699273
Kholofelo Tladi <tladi.kholofelo@gmail.com>
2024-12-22 20:52:15 +0200

    Add alx-listing-app files

---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+// next.js-app/pages/index.tsx
+import React from 'react';
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1>Welcome to ALX Listing App!</h1>
+    </div>
+  );
+};
+
+export default HomePage;
